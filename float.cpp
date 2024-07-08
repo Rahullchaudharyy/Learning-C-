1f7db6525ded63475ea0825b16f8e2575f16b99f
Rahullchaudharyy <user.rahullchaudharryy@gmail.com>
2024-07-08 10:53:32 +0530

    C++ learning float

---
+++ float.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std ; 
+
+int main (){
+
+
+    printf("Float size is %ld\n",sizeof(long double)*8);
+
+
+    float my_p_value = 20.2 + 20.2;
+
+  
+  if (my_p_value == 40.400002)
+  {
+    puts("Hellò you got it right ");
+  }else{
+   puts("something ");
+
+  }
+    printf("my p value is %1.223f\n",my_p_value);
+  
+
+
+
+    return 0 ;
+
+
+}
