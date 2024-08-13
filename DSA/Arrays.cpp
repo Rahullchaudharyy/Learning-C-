661b13a3e08cc979bc5815fc716a9536fdde1e19
Rahullchaudharyy <user.rahullchaudharryy@gmail.com>
2024-08-13 13:40:41 +0530

    Arrays Pending...

---
+++ DSA/Arrays.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+   int a[10] ={
+
+    1,2,
+    3,4,
+    5,6,
+         };
+         
+   int B[10] ={1,2,3,4,5};
+   int C[10] ={1,2,3,4,5};
+   int D[10] ={1,2,3,4,5};
+   int E[10] ={1,2,3,4,5};
+   int F[10] ={1,2,3,4,5};
+
+   cout<< a[5]<<endl; 
+   cout<< B<<endl; 
+   cout<< C<<endl; 
+   cout<< D<<endl; 
+   cout<< E<<endl; 
+   cout<< F<<endl; 
+
+  char Name [] = {'a','b','c','d','e'};
+  cout << sizeof Name char;
+
+
+   return 0 ;
+
+}
