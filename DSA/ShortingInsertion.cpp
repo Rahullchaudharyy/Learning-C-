e46980cd1a4e9e88588ae4b520f9045c52e3ec56
Rahullchaudharyy <user.rahullchaudharryy@gmail.com>
2024-08-26 09:02:46 +0530

    []

--- DSA/ShortingInsertion.cpp
+++ DSA/ShortingInsertion.cpp
@@ -5,26 +5,19 @@ int main(){
 
 
 
+    int Array[1000] ;
 
-
-
-    int Array[5] = {4, 1, 3, 2, 5};
-
-    for (int i = 1; i < 5; i++)  // Start loop from 1 to avoid index -1
+    int n ; 
+    cout<< "Please enter the length of an arrays :- ";
+    cin>> n;
+    for (int i = 0; i < n; i++)
     {
-        if (Array[i] < Array[i - 1])
-        {
-            swap(Array[i], Array[i - 1]);
-        }
+       cin>>Array[i];
     }
+    
 
-    for (int j = 0; j < 5; j++)  // Changed loop to < 5 to print all elements
-    {
-        cout << Array[j] << " ";
-    }
 
-    return 0;
-
+   
  
 
 }
