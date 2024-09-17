0b08c75526a9467613e783153b37481f7ed37947
Rahullchaudharyy <rahullchaudharryy@gmail.com>
2024-09-17 21:09:43 +0530

    experoment

---
+++ C++/ChaiCode/data.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+
+
+
+    int teaLeaves = 4 ;
+    float waterTEmprature = 45.9;
+    double priceOFtea = 555.90345678987654323456789;
+    char grade = 'A';
+    bool Isready = false;
+
+    cout << priceOFtea << endl; 
+
+
+
+
+    return 0 ;
+}
